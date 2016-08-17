5142c453aaf6749fac11f1b72254bf3ca845f608
comtu <comtu@vip.qq.com>
2016-08-17 17:52:01 +0800

    delete file

--- public/res/game/plane/windowTocanvas.js
+++
@@ -1,9 +0,0 @@
-	//坐标转换方法
-			function windowToCanvas(element,x,y){
-				//元素独有的获取坐标的方法
-				var box=element.getBoundingClientRect();
-				var cx=x-box.left;
-				var cy=y-box.top;
-				return {x:cx,y:cy}
-			
-			}
